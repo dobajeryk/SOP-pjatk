02917a9d6601b87beba8087ac419e37e796a5515
Eryk Dobaj <dobajeryk@gmail.com>
2021-05-30 18:40:26 +0200

    Dodana instrukcja jak uruchomic skrypt - zadanie01

--- kolokwia/zadanie01.sh
+++ kolokwia/zadanie01.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-#TRESC
+# Treść
 #Napisz skrypt, który wypisze nazwy tych katalogów z katalogu podanego jako argument, w których
 #są przynajmniej trzy pliki zaczynające się na dużą literę z rozszerzeniem exe lub sh lub bez
 #rozszerzenia, które można uruchomić.
+
+#Jak odpalić?
+#./zadanie01.sh zadanie01
 
 KATALOG=$1
 KATALOGI=$(find $KATALOG -type d)
